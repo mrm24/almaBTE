b9803eb3f1062aeca9951dbac5e72197cd6c31d4
mrm24 <marti_94@hotmail.com>
2026-06-20 11:12:30 +0200

    Changed to binaries

--- src/bulk_hdf5.cpp
+++ src/bulk_hdf5.cpp
@@ -20,7 +20,7 @@
 #include <H5Cpp.h>
 #include <bulk_hdf5.hpp>
 #include <fstream>
-#include <msgpack.hpp>
+// #include <msgpack.hpp>
 #include <filesystem>
 
 // There seems to be no way to avoid this "using" without
@@ -469,15 +469,29 @@ void save_bulk_hdf5(const char* filename,
     if (nprocs_4ph == 0) return;
 
     std::string filename_4ph(filename);
-    filename_4ph.replace(filename_4ph.find(".h5"), 3, ".4ph.msgpack");
+    filename_4ph.replace(filename_4ph.find(".h5"), 3, ".4ph");
 
     /// The master
     comm.barrier();
     if (comm.rank() == 0) {
         std::ofstream ofs(filename_4ph, std::ios::binary | std::ios::trunc);
         if (!ofs) throw std::runtime_error("Failed to open file for writting: " + filename_4ph);
-        msgpack::pack(ofs, static_cast<std::uint64_t>(nprocs_4ph));
-        for (const auto& elem : processes_4ph) msgpack::pack(ofs, elem);
+        // msgpack::pack(ofs, static_cast<std::uint64_t>(nprocs_4ph));
+        // for (const auto& elem : processes_4ph) msgpack::pack(ofs, elem);
+        ofs << nprocs_4ph;
+        for (const auto& elem : processes_4ph) {
+            ofs << elem.c;
+            for (const auto& x : elem.q) ofs << x;
+            for (const auto& x : elem.alpha) ofs << x;
+            ofs << static_cast<int>(elem.type)
+                << elem.domega
+                << elem.sigma
+                << elem.vp2_computed
+                << elem.gaussian_computed
+                << elem.vp2
+                << elem.gaussian;
+        }
+
         ofs.flush();
         ofs.close();
     }
@@ -488,7 +502,19 @@ void save_bulk_hdf5(const char* filename,
         if (iproc == comm.rank()) {
             std::ofstream ofs(filename_4ph, std::ios::binary | std::ios::app);
             if (!ofs) throw std::runtime_error("Failed to open file for writting: " + filename_4ph);
-            for (const auto& elem : processes_4ph) msgpack::pack(ofs, elem);
+            // for (const auto& elem : processes_4ph) msgpack::pack(ofs, elem);
+            for (const auto& elem : processes_4ph) {
+                ofs << elem.c;
+                for (const auto& x : elem.q) ofs << x;
+                for (const auto& x : elem.alpha) ofs << x;
+                ofs << static_cast<int>(elem.type)
+                    << elem.domega
+                    << elem.sigma
+                    << elem.vp2_computed
+                    << elem.gaussian_computed
+                    << elem.vp2
+                    << elem.gaussian;
+            }
             ofs.flush();
             ofs.close();
         }
@@ -892,55 +918,60 @@ load_bulk_hdf5(const char* filename, const boost::mpi::communicator& comm) {
     auto nruter5 = alma::make_unique<std::vector<Fourph_process>>();
 
     std::string filename_4ph(filename);
-    filename_4ph.replace(filename_4ph.find(".h5"), 3, ".4ph.msgpack");
+    filename_4ph.replace(filename_4ph.find(".h5"), 3, ".4ph");
 
     auto FourPhononsFiles = std::filesystem::exists(filename_4ph);
+
+    constexpr std::size_t bytes_4ph = 9 * sizeof(std::size_t) + 
+                                      sizeof(int) + 
+                                      2 * sizeof(bool) + 
+                                      4 * sizeof(double);
     
     if (FourPhononsFiles) {
         std::ifstream ifs(filename_4ph, std::ios::binary);
         if (!ifs) throw std::runtime_error("Failed to open file for reading: " + filename_4ph);
 
-        msgpack::unpacker pac;
-        auto read_more = [&](std::size_t want) {
-            // Ensure unpacker has room, read from file into its internal buffer
-            pac.reserve_buffer(want);
-            ifs.read(pac.buffer(), want);
-            std::streamsize got = ifs.gcount();
-            if (got <= 0) return std::size_t(0);
-            pac.buffer_consumed(static_cast<std::size_t>(got));
-            return static_cast<std::size_t>(got);
-        };
+        std::size_t n4ph, c;
+        std::array<size_t,4> q, alpha;
+        double sigma, vp2, gaussian, domega;
+        int type_int;
+        bool vp2_computed, gaussian_computed;
+        alma::fourph_type type;
 
-        constexpr std::size_t chunk_size = 5 << 20; // 5 MB at a time, it is more than enough to hold several 4ph objects
+        /// Read the number of 4ph processes
+        ifs >> n4ph;
 
-        /// Read the size
-        msgpack::object_handle oh;
-        while (!pac.next(oh)) {
-            if (read_more(chunk_size) == 0)
-                throw std::runtime_error("Unexpected EOF reading from " + filename_4ph);
-        }
-        std::size_t n4ph = oh.get().as<std::uint64_t>();
-
+        /// Get what is stored here
         auto limits = alma::my_jobs(n4ph, comm.size(), comm.rank());
         nruter5->reserve(limits[1] - limits[0]);
+        
+        /// Here move the ifs to the limit[0] entry
+        /// each 4 ph object occupies bytes_4ph
+        /// Each MPI rank only reads a section. The 
+        /// file is processed in parallel
+        ifs.seekg(sizeof(std::size_t) + static_cast<std::streamoff>(limits[0]) * bytes_4ph);
 
-        // Skip records before limits[0] 
-        for (std::size_t i = 0; i < limits[0]; ++i) {
-            while (!pac.next(oh)) {
-                if (read_more(chunk_size) == 0)
-                    throw std::runtime_error("Unexpected EOF while skipping entries in " + filename_4ph);
-            }
-        }
+        /// Read objects on my section
+        for (std::size_t i = limits[0]; i < limits[1]; i++) {
+            ifs >> c;
+            for (auto& x : q) ifs >> x;
+            for (auto& x : alpha) ifs >> x;
 
-        // Read this rank's chunk [limits[0], limits[1]) ---
-        for (std::size_t i = limits[0]; i < limits[1]; ++i) {
-            while (!pac.next(oh)) {
-                if (read_more(chunk_size) == 0)
-                    throw std::runtime_error("Unexpected EOF while reading rank's (" + 
-                        std::to_string(comm.rank()) + ") entries in " + filename_4ph);
-            }
-            Fourph_process p(0, {0,0,0,0},{0,0,0,0}, alma::fourph_type::recombination, 0., 0.);
-            oh.get().convert(p);
+            ifs >> type_int
+                >> domega
+                >> sigma
+                >> vp2_computed
+                >> gaussian_computed
+                >> vp2
+                >> gaussian;
+            type = static_cast<alma::fourph_type>(type_int);
+
+            Fourph_process p(c, q, alpha, type, domega, sigma);
+            p.vp2_computed      = vp2_computed;
+            p.gaussian_computed = gaussian_computed;
+            p.gaussian          = gaussian;
+            p.vp2               = vp2;
+
             nruter5->emplace_back(p);
         }
 
@@ -1458,3 +1489,43 @@ void load_ifcs_subgroup(const char* filename,
 }
 
 } // namespace alma
+
+/// TODO(mrm) Change to MSGPACK
+// msgpack::unpacker pac;
+// auto read_more = [&](std::size_t want) {
+//     // Ensure unpacker has room, read from file into its internal buffer
+//     pac.reserve_buffer(want);
+//     ifs.read(pac.buffer(), want);
+//     std::streamsize got = ifs.gcount();
+//     if (got <= 0) return std::size_t(0);
+//     pac.buffer_consumed(static_cast<std::size_t>(got));
+//     return static_cast<std::size_t>(got);
+// };
+// constexpr std::size_t chunk_size = 5 << 20; // 5 MB at a time, it is more than enough to hold several 4ph objects
+// /// Read the size
+// msgpack::object_handle oh;
+// while (!pac.next(oh)) {
+//     if (read_more(chunk_size) == 0)
+//         throw std::runtime_error("Unexpected EOF reading from " + filename_4ph);
+// }
+// std::size_t n4ph = oh.get().as<std::uint64_t>();
+// auto limits = alma::my_jobs(n4ph, comm.size(), comm.rank());
+// nruter5->reserve(limits[1] - limits[0]);
+// // Skip records before limits[0] 
+// for (std::size_t i = 0; i < limits[0]; ++i) {
+//     while (!pac.next(oh)) {
+//         if (read_more(chunk_size) == 0)
+//             throw std::runtime_error("Unexpected EOF while skipping entries in " + filename_4ph);
+//     }
+// }
+// // Read this rank's chunk [limits[0], limits[1]) ---
+// for (std::size_t i = limits[0]; i < limits[1]; ++i) {
+//     while (!pac.next(oh)) {
+//         if (read_more(chunk_size) == 0)
+//             throw std::runtime_error("Unexpected EOF while reading rank's (" + 
+//                 std::to_string(comm.rank()) + ") entries in " + filename_4ph);
+//     }
+//     Fourph_process p(0, {0,0,0,0},{0,0,0,0}, alma::fourph_type::recombination, 0., 0.);
+//     oh.get().convert(p);
+//     nruter5->emplace_back(p);
+// }
